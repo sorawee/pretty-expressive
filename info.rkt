357057239c691c1d7c70c50235a74570441095b8
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2023-09-09 14:31:09 -0700

    version: fix version

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/pretty-expressive.scrbl" ())))
 (define pkg-desc "A pretty expressive printer")
-(define version "1.0.0")
+(define version "1.0")
 (define pkg-authors '(sorawee))
 (define license '(Apache-2.0 OR MIT))
